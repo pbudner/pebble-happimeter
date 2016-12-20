f51c4d218e248bfe4b8c86960b914cc6bf5e0119
iuyyoy <iuynet@yeah.net>
2016-12-20 22:50:35 +0800

    Change codes to fit intro_window.

--- src/c/windows/tree_window.c
+++ src/c/windows/tree_window.c
@@ -3,41 +3,50 @@
 static Window *treeWindow;
 static GBitmap *treeImage;
 static BitmapLayer *treeImageLayer;
-static uint32_t key;
 static uint32_t counter;
 
-// dedicated button manager from here ---
-void back_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
-  Window *window = (Window *)context;
-  APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
+// // dedicated button manager from here ---
+// void back_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
+//   Window *window = (Window *)context;
+//   APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
+//   window_stack_pop_all(true);
+// }
+//
+// // dedicated button manager from here ---
+// void down_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
+//   Window *window = (Window *)context;
+//   APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
+//   window_stack_pop_all(true);
+// }
+//
+// // dedicated button manager from here ---
+// void select_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
+//   Window *window = (Window *)context;
+//   APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
+//   window_stack_pop_all(true);
+// }
+//
+// // dedicated button manager from here ---
+// void up_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
+//   Window *window = (Window *)context;
+//   APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
+//   window_stack_pop_all(true);
+// }
+/***********************************
+* Any button single click handler  *
+***********************************/
+void tree_single_click_handler(ClickRecognizerRef recognizer, void *context)
+{
+  APP_LOG(APP_LOG_LEVEL_INFO, "One button is pressed on tree_window. Exiting now!");
   window_stack_pop_all(true);
 }
 
-// dedicated button manager from here ---
-void down_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
-  Window *window = (Window *)context;
-  APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
-  window_stack_pop_all(true);
-}
-
-// dedicated button manager from here ---
-void select_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
-  Window *window = (Window *)context;
-  APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
-  window_stack_pop_all(true);
-}
-
-// dedicated button manager from here ---
-void up_single_click_handler_tree(ClickRecognizerRef recognizer, void *context) {
-  Window *window = (Window *)context;
-  APP_LOG(APP_LOG_LEVEL_INFO, "Back button is pressed on missingconfig_window. Exiting now!");
-  window_stack_pop_all(true);
-}
-void config_provider_tree(Window *window) {
-  window_single_click_subscribe(BUTTON_ID_BACK, back_single_click_handler_tree);  
-   window_single_click_subscribe(BUTTON_ID_SELECT, down_single_click_handler_tree);  
-   window_single_click_subscribe(BUTTON_ID_DOWN, up_single_click_handler_tree);  
-   window_single_click_subscribe(BUTTON_ID_UP, select_single_click_handler_tree);  
+void config_provider_tree(Window *window)
+{
+  window_single_click_subscribe(BUTTON_ID_BACK, back_single_click_handler_tree);
+  window_single_click_subscribe(BUTTON_ID_SELECT, down_single_click_handler_tree);
+  window_single_click_subscribe(BUTTON_ID_DOWN, up_single_click_handler_tree);
+  window_single_click_subscribe(BUTTON_ID_UP, select_single_click_handler_tree);
 }
 
 /***********************************
@@ -48,33 +57,33 @@ void tree_window_load(Window *window)
   int _activation, _pleasant;
   _activation = getActivation();
   _pleasant = getPleasant();
-  
+
   APP_LOG(APP_LOG_LEVEL_INFO, "Pleasant value is %d", _pleasant);
   APP_LOG(APP_LOG_LEVEL_INFO, "Activation value is %d", _activation);
   upload_mood(_pleasant, _activation);
   Layer *window_layer = window_get_root_layer(window);
   APP_LOG(APP_LOG_LEVEL_INFO, "%ld", persist_read_int(counter));
 
-  treeWindow = window_create();
+  // treeWindow = window_create(); //apper second times
 #ifndef PBL_SDK_3
   window_set_fullscreen(treeWindow, true);
 #endif
 
-  if (!persist_exists(key))
+  if (!persist_exists(TREE_KEY))
   {
-    key = 0;
     counter = 1;
-    persist_write_int(key, counter);
+    persist_write_int(TREE_KEY, counter);
     //treeImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TREE1);
   }
   else
   {
-    counter = persist_read_int(key);
+    counter = persist_read_int(TREE_KEY);
+    counter += 1;
+    persist_write_int(TREE_KEY, counter);
     counter = counter % 4;
   }
 
   switch (counter)
-
   {
   case 1:
 #if defined(PBL_PLATFORM_BASALT)
@@ -82,8 +91,6 @@ void tree_window_load(Window *window)
 #else
     treeImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TREE1_Black_White);
 #endif
-    counter = counter + 1;
-    persist_write_int(key, counter);
     break;
 
   case 2:
@@ -92,8 +99,6 @@ void tree_window_load(Window *window)
 #else
     treeImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TREE2_Black_White);
 #endif
-    counter = counter + 1;
-    persist_write_int(key, counter);
     break;
 
   case 3:
@@ -102,8 +107,6 @@ void tree_window_load(Window *window)
 #else
     treeImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TREE3_Black_White);
 #endif
-    counter = counter + 1;
-    persist_write_int(key, counter);
     break;
 
   case 0:
@@ -112,8 +115,6 @@ void tree_window_load(Window *window)
 #else
     treeImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TREE4_Black_White);
 #endif
-    counter = counter + 1;
-    persist_write_int(key, counter);
     break;
   }
 
@@ -126,7 +127,7 @@ void tree_window_load(Window *window)
   bitmap_layer_set_bitmap(treeImageLayer, treeImage);
   bitmap_layer_set_compositing_mode(treeImageLayer, GCompOpSet);
   layer_add_child(window_layer, bitmap_layer_get_layer(treeImageLayer));
-  
+
   // override back_button_manager
   window_set_click_config_provider(window, (ClickConfigProvider) config_provider_tree);
 }
--- src/c/windows/tree_window.h
+++ src/c/windows/tree_window.h
@@ -3,6 +3,8 @@
 #include "../main.h"
 #include "../modules/data_manager.h"
 
+#define TREE_KEY 0
+
 extern void init_tree_window(void);
 extern void deinit_tree_window(void);
 extern Window *tree_window_get_window(void);
